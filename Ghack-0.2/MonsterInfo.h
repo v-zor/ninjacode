01c00a1045f7d463c15a6451ab017a80e7a0fbb3
U-rogue\buble <buble@rogue.(none)>
2014-03-28 16:04:06 -0700

    cleanup

--- Ghack-0.2/MonsterInfo.h
+++ Ghack-0.2/MonsterInfo.h
@@ -2,7 +2,6 @@
 #import <AppKit/AppKit.h>
 //#include "ghack.h"
 /*
-Copyright (C) 2013 Johan Ceuppens 
 Copyright (C) 2013 v-zor
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
--- Ghack-0.2/Views.h
+++ Ghack-0.2/Views.h
@@ -3,7 +3,6 @@
 #include "MonsterInfo.h"
 
 /*
-Copyright (C) 2013 Johan Ceuppens 
 Copyright (C) 2013 v-zor
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
